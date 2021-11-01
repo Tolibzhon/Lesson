c9356b0cc617099a6f7d395f4388c753dcd11ec8
Tolibzhon <sidikovtolibjon2000@gmail.com>
2021-11-01 18:40:04 +0600

    Fib

--- lesson1/bin/homeworkFib.dart
+++ lesson1/bin/homeworkFib.dart
@@ -1,13 +1,12 @@
 void main() {
   int a = 77;
   int n1 = 0, n2 = 1, n3;
-  // print(n1);
-  // print(n2);
 
   for (int i = 2; i <= a; i++) {
     n3 = n1 + n2;
-    print('$n3');
+    // print('$n3');
     n1 = n2;
     n2 = n3;
   }
+  print('$n3');
 }
