40d6f377a919c8eb41010a6cfc0653d5fec40934
Tolibzhon <sidikovtolibjon2000@gmail.com>
2021-12-01 17:09:23 +0600

    Flutter 1,2 home work

--- lesson1/bin/homework16.dart
+++ lesson1/bin/homework16.dart
@@ -1,70 +1,70 @@
-import 'dart:io';
+// import 'dart:io';
 
-class Fabrics {
-  String? name;
-  int? price;
+// class Fabrics {
+//   String? name;
+//   int? price;
 
-  void getdata() {
-    stdout.write("Введите название ткани:");
-    this.name = stdin.readLineSync() ?? "";
-    stdout.write("Введите цену:");
-    this.price = int.parse(stdin.readLineSync() ?? "0");
-  }
+//   void getdata() {
+//     stdout.write("Введите название ткани:");
+//     this.name = stdin.readLineSync() ?? "";
+//     stdout.write("Введите цену:");
+//     this.price = int.parse(stdin.readLineSync() ?? "0");
+//   }
 
-  void getInfo() {
-    print("Название ткани: $name , Цена: $price");
-  }
-}
+//   void getInfo() {
+//     print("Название ткани: $name , Цена: $price");
+//   }
+// }
 
-class InfoProducer extends Fabrics {
-  String? info;
-  @override
-  void getdata() {
-    // TODO: implement getdata
-    super.getdata();
-    stdout.write("Введите производителя:");
-    this.info = stdin.readLineSync() ?? "";
-  }
+// class InfoProducer extends Fabrics {
+//   String? info;
+//   @override
+//   void getdata() {
+//     // TODO: implement getdata
+//     super.getdata();
+//     stdout.write("Введите производителя:");
+//     this.info = stdin.readLineSync() ?? "";
+//   }
 
-  @override
-  void getInfo() {
-    // TODO: implement getInfo
-    super.getInfo();
-    print("Информация о производителе ткани-$info");
-  }
-}
+//   @override
+//   void getInfo() {
+//     // TODO: implement getInfo
+//     super.getInfo();
+//     print("Информация о производителе ткани-$info");
+//   }
+// }
 
-class InfoCom extends Fabrics {
-  String? composition;
-  int? metr;
-  @override
-  void getdata() {
-    // TODO: implement getdata
-    super.getdata();
-    stdout.write("Введите состав пряжи:");
-    this.composition = stdin.readLineSync() ?? "";
-    stdout.write("Введите метр в мотке:");
-    this.metr = int.parse(stdin.readLineSync() ?? "0");
-  }
+// class InfoCom extends Fabrics {
+//   String? composition;
+//   int? metr;
+//   @override
+//   void getdata() {
+//     // TODO: implement getdata
+//     super.getdata();
+//     stdout.write("Введите состав пряжи:");
+//     this.composition = stdin.readLineSync() ?? "";
+//     stdout.write("Введите метр в мотке:");
+//     this.metr = int.parse(stdin.readLineSync() ?? "0");
+//   }
 
-  @override
-  void getInfo() {
-    // TODO: implement getInfo
-    super.getInfo();
-    print("Состав пряжи-$composition\nМетр в мотке-$metr");
-  }
-}
+//   @override
+//   void getInfo() {
+//     // TODO: implement getInfo
+//     super.getInfo();
+//     print("Состав пряжи-$composition\nМетр в мотке-$metr");
+//   }
+// }
 
-void main() {
-  // Fabrics fabrics = Fabrics();
-  // fabrics.getdata();
+// void main() {
+//   // Fabrics fabrics = Fabrics();
+//   // fabrics.getdata();
 
-  InfoProducer infoprd = InfoProducer();
-  infoprd.getdata();
+//   InfoProducer infoprd = InfoProducer();
+//   infoprd.getdata();
 
-  InfoCom infocom = InfoCom();
-  infocom.getdata();
+//   InfoCom infocom = InfoCom();
+//   infocom.getdata();
 
-  infoprd.getInfo();
-  infocom.getInfo();
-}
+//   infoprd.getInfo();
+//   infocom.getInfo();
+// }
